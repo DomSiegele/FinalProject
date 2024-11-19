44ddcf5a172e64b89b0e92f98a36de296ee081d9
DomSiegele <dsiegele@unm.edu>
2024-11-19 14:44:29 -0700

    Update data.py

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 st.title("Excel Data Visualization App")
 
 # File uploader for Excel
-uploaded_file = st.file_uploader("Water_Data_Clean", type=["xlsx"])
+uploaded_file = st.file_uploader("Water_Data_Clean1.csv", type=["csv"])
 
 if uploaded_file is not None:
     # Read the Excel file
